bc4f9595fb27bd6148c2f7375484efa5374b20e4
JohnGrib.lee <johngrib.lee@greenlabs.co.kr>
2022-04-23 23:39:09 +0900

    Module 2: books 데이터를 읽는다

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,1 +1,8 @@
-(ns user)
+(ns user
+  (:require [clojure.data.json :as json]
+            [clojure.pprint :refer [pprint]]))
+
+(def books (json/read-str (slurp "resources/data/books.json")))
+
+(comment
+  (pprint (first books)))
