63b317ec2b7f4baac798d5b75bbec03e56737ea4
JohnGrib.lee <johngrib.lee@greenlabs.co.kr>
2022-04-24 01:50:18 +0900

    Module 3: book_by_id 조회가 가능하도록 user.clj 작성

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,8 +1,42 @@
 (ns user
   (:require [clojure.data.json :as json]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [clojure.edn :as edn]
+            [com.walmartlabs.lacinia :refer [execute]]
+            [com.walmartlabs.lacinia.util :refer [attach-resolvers]]
+            [com.walmartlabs.lacinia.schema :as schema]
+            [got.schema :refer [load-schema]]))
 
-(def books (json/read-str (slurp "resources/data/books.json")))
+(def books (-> (slurp "resources/data/books.json")
+               (json/read-str :key-fn keyword)
+               #_(index-by :Id)))
+
+(def characters (-> (slurp "resources/data/characters.json")
+                    (json/read-str :key-fn keyword)
+                    count))
+
+(def houses (-> (slurp "resources/data/houses.json")
+                (json/read-str :key-fn keyword)
+                count))
+
+(def got-schema (load-schema))
+
+(defn q
+  [query-string]
+  (execute got-schema query-string nil nil))
 
 (comment
-  (pprint (first books)))
+  (pprint books)
+  (pprint (first books))
+  (count (first books))
+
+  (schema/compile (-> (slurp "resources/schema.edn")
+                      edn/read-string
+                      (attach-resolvers {})))
+
+  (load-schema)
+
+  (q "{ book_by_id(id: 11) { Id Name }}") ;; {:data {:book_by_id {:Id 11, :Name "The World of Ice and Fire"}}}
+  )
+
+
