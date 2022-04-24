abbd7b89eb4254aded5faa9c6bbfa7d3e19d6981
JohnGrib.lee <johngrib.lee@greenlabs.co.kr>
2022-04-25 00:05:34 +0900

    Module 5: House, Character에 해당하는 resolver 추가

--- src/main/got/schema.clj
+++ src/main/got/schema.clj
@@ -1,10 +1,10 @@
 (ns got.schema
   (:require [clojure.data.json :as json]
-            [clojure.pprint :refer [pprint]]
             [clojure.edn :as edn]
-            [com.walmartlabs.lacinia.util :refer [attach-resolvers]]
+            [clojure.pprint :refer [pprint]]
+            [com.walmartlabs.lacinia :refer [execute]]
             [com.walmartlabs.lacinia.schema :as schema]
-            [com.walmartlabs.lacinia :refer [execute]]))
+            [com.walmartlabs.lacinia.util :refer [attach-resolvers]]))
 
 ;; helper functions
 (defn index-by
@@ -16,10 +16,13 @@
                (json/read-str :key-fn keyword)
                (index-by :Id)))
 
-(comment
-  (pprint books)
-  (pprint (first books))
-  (count (first books)))
+(def houses (-> (slurp "resources/data/houses.json")
+                (json/read-str :key-fn keyword)
+                (index-by :Id)))
+
+(def characters (-> (slurp "resources/data/characters.json")
+                    (json/read-str :key-fn keyword)
+                    (index-by :Id)))
 
 ;; resolvers
 (defn get-book
@@ -27,10 +30,30 @@
   [context {:keys [id]} value]
   (get books id))
 
+(defn get-character
+  "등장인물 정보를 리턴합니다."
+  [context {:keys [id]} value]
+  (get characters id))
+
+(defn get-house
+  [id]
+  (get houses id))
+
+(defn get-allegiances
+  [context args {:keys [Allegiances]}]
+  (map get-house Allegiances))
+
+(defn get-books
+  [context args {:keys [Books]}]
+  (map #(get books %) Books))
+
 (defn resolver-map
   "resolver 모음"
   []
-  {:query/book-by-id get-book})
+  {:query/book-by-id      get-book
+   :query/character-by-id get-character
+   :Character/Allegiances get-allegiances
+   :Character/Books       get-books})
 
 ;; schema
 (defn load-schema
@@ -43,4 +66,16 @@
       schema/compile))
 
 (comment
+  (pprint books)
+  (pprint (first books))
+  (count (first books))
+
+  (pprint houses)
+  (pprint (first houses))
+  (count (first houses))
+
+  (pprint characters)
+  (pprint (first characters))
+  (count (first characters))
+
   (load-schema))
